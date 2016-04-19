c5d541e953c4eb8e8d59a64a5279600d150a5dad
poleha <pass1191>
2016-04-19 16:58:00 +0300

    cloneState added for user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -10,14 +10,22 @@ const initialState = {
 
 };
 
+function cloneState(state) {
+    return Object.assign({}, state);
+}
+
+
 export default function user(state = initialState, action) {
 
     switch (action.type) {
         case USER_LOGIN_START:
+            state = cloneState(state);
             return { ...state, logging: true};
         case USER_LOGIN_SUCCESS:
+            state = cloneState(state);
             return { ...state, logging: false, logged: true, userId: action.payload.user_id, userName:action.payload.username};
         case USER_LOGIN_FAIL:
+            state = cloneState(state);
             return { ...state, logging: false};
         default:
             return state;

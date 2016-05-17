e12135078e2d089fd43c2b25ae9d6bdaf2806988
poleha <pass1191>
2016-05-17 16:47:29 +0300

    Monkey patch removed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
-import * as patches from 'monkey'
 
 const store = configureStore();
 export const history = syncHistoryWithStore(browserHistory, store);

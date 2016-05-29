1e2e89bc1d6e8393bacb9f9690ab6b3a1febf724
poleha <pass1191>
2016-05-29 18:11:53 +0300

    Styles enabled

--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux'
 import { ReduxAsyncConnect } from 'redux-async-connect'
 import routes from '../../routes'
 import { Router } from 'react-router'
-//import './styles.less'
+import './styles.less'
 
 export default class Root extends Component {
+    
     render() {
         const { store, history } = this.props
         return (
